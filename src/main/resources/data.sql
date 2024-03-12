562b98aff8d5d1c04cc55fc24b6df3a16533c70a
dragon0622 <iamdragon.dev@gmail.com>
2024-03-12 13:33:56 +0900

    Delete: create sql 삭제

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-create table article (
-                         id BIGINT AUTO_INCREMENT primary key,
-                         title varchar(255) not null,
-                         content varchar(255) not null
-);
+-- create table article not exists article (
+--                          id BIGINT AUTO_INCREMENT primary key,
+--                          title varchar(255) not null,
+--                          content varchar(255) not null
+-- );
 
 INSERT INTO article (title, content) VALUES ('블로그 제목', '블로그 내용');
 INSERT INTO article (title, content) VALUES ('블로그 제목2', '블로그 내용2');
