f3f0bb9d76c9723b9136e5b6894bf6ce4c4cb85f
dragon0622 <iamdragon.dev@gmail.com>
2024-03-12 13:33:26 +0900

    Feat: 모든 테이블 조회 기능 추가

--- src/main/java/com/estsoft/blogjpa/controller/BlogController.java
+++ src/main/java/com/estsoft/blogjpa/controller/BlogController.java
@@ -7,10 +7,9 @@ import com.estsoft.blogjpa.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BlogController {
@@ -24,5 +23,20 @@ public class BlogController {
     public ResponseEntity<ArticleResponse> addArticle(@RequestBody AddArticleRequest request){
         Article article = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(article.toResponse());
+    }
+
+    //=@GetMapping("ari/articles")
+    @RequestMapping(value="api/allarticles", method = RequestMethod.GET)
+    public ResponseEntity<List<ArticleResponse>> showArticle(){
+        List<Article> articleList = blogService.findAll();
+        List<ArticleResponse> articleResponseList = articleList
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        return ResponseEntity.ok(articleResponseList);
+    }
+
+    @GetMapping("api/articles/{id}")
+    public ResponseEntity<ArticleResponse> showOneArticle(@RequestParam(String title)){
     }
 }
--- src/main/java/com/estsoft/blogjpa/service/BlogService.java
+++ src/main/java/com/estsoft/blogjpa/service/BlogService.java
@@ -5,6 +5,8 @@ import com.estsoft.blogjpa.dto.AddArticleRequest;
 import com.estsoft.blogjpa.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BlogService {
     private final BlogRepository blogRepository;
@@ -15,5 +17,9 @@ public class BlogService {
 
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
     }
 }
