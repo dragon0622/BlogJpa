c3187136a730e0103a5fd3605c48e3c28e2398e7
dragon0622 <iamdragon.dev@gmail.com>
2024-03-12 10:37:00 +0900

    Feat: BlogService 내 save 메소드 구현

---
+++ src/main/java/com/estsoft/blogjpa/service/BlogService.java
@@ -0,0 +1,19 @@
+package com.estsoft.blogjpa.service;
+
+import com.estsoft.blogjpa.domain.Article;
+import com.estsoft.blogjpa.dto.AddArticleRequest;
+import com.estsoft.blogjpa.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public BlogService(BlogRepository blogRepository) {
+        this.blogRepository = blogRepository;
+    }
+
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
+}
