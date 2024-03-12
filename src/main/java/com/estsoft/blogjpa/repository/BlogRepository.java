3754de864f516f389e07298cfa106194af21163b
dragon0622 <iamdragon.dev@gmail.com>
2024-03-12 09:49:31 +0900

    Feat: Entity 구성을 위한 인터페이스 추가
    엔티티 Article, PK타입 Long 매개변수로 추가

---
+++ src/main/java/com/estsoft/blogjpa/repository/BlogRepository.java
@@ -0,0 +1,8 @@
+package com.estsoft.blogjpa.repository;
+
+import com.estsoft.blogjpa.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+    //상속을 받으면서 이 레파지토리를 사용할땓, JpaRepository에서 제공하는 여러 메소드 사용 가능
+}
