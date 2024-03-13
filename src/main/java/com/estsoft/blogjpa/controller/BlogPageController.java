5cae115f7f66180348b0ad355803e4b58dac877c
dragon0622 <iamdragon.dev@gmail.com>
2024-03-13 09:23:02 +0900

    Feat: BlogPageController 추가

---
+++ src/main/java/com/estsoft/blogjpa/controller/BlogPageController.java
@@ -0,0 +1,28 @@
+package com.estsoft.blogjpa.controller;
+
+import com.estsoft.blogjpa.dto.ArticleViewResponse;
+import com.estsoft.blogjpa.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BlogPageController {
+    private BlogService blogService;
+
+    public BlogPageController(BlogService blogService) {
+        this.blogService = blogService;
+    }
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);   // model에 블로그 글 리스트 저장
+
+        return "articleList";   // articleList.html라는 뷰 조회
+    }
+}
