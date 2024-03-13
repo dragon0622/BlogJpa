17326b692fa5484da7e041d618d16eb9b7bd2f86
dragon0622 <iamdragon.dev@gmail.com>
2024-03-13 09:36:19 +0900

    Feat: 어노테이션 추가
    @EnableJpaAuditing  //JPA Auditing 기능 활성화

--- src/main/java/com/estsoft/blogjpa/BlogJpaApplication.java
+++ src/main/java/com/estsoft/blogjpa/BlogJpaApplication.java
@@ -2,7 +2,9 @@ package com.estsoft.blogjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing  //JPA Auditing 기능 활성화 ( 엔티티 변경 이력 추적, 생성일자 및 수정일자 자동 설정)
 @SpringBootApplication
 public class BlogJpaApplication {
 
