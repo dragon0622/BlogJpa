4ea7942ee84c79db9588ca81ab5b8b8fd4d0f20b
dragon0622 <iamdragon.dev@gmail.com>
2024-03-16 00:00:10 +0900

    Feat: Comment class Builder 추가

--- src/main/java/com/estsoft/blogjpa/domain/Comment.java
+++ src/main/java/com/estsoft/blogjpa/domain/Comment.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -29,5 +30,11 @@ public class Comment {
     @CreatedDate    //엔티티가 생성될때 생성 시간을 테이블의 created_at 컬럼에 저장하겠다.
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Builder
+    public Comment(Long articleId, String body){
+        this.articleId = articleId;
+        this.body = body;
+    }
 
 }
