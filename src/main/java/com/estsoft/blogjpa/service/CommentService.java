df2e4557466ea0fd79a0578cca55df32f0528cf6
dragon0622 <iamdragon.dev@gmail.com>
2024-03-17 23:38:34 +0900

    Feat: CommentService 함수 추가

---
+++ src/main/java/com/estsoft/blogjpa/service/CommentService.java
@@ -0,0 +1,27 @@
+package com.estsoft.blogjpa.service;
+
+import com.estsoft.blogjpa.domain.Comment;
+import com.estsoft.blogjpa.dto.CommentDTO;
+import com.estsoft.blogjpa.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public Comment createComment(Long articleId, CommentDTO commentDTO){
+        Comment comment = commentDTO.toEntity(articleId);
+        return commentRepository.save(comment);
+    }
+
+    public Comment findCommentById(Long articleId, Long commentId) {
+
+        return commentRepository.findByArticleId(articleId,commentId);
+    }
+}
