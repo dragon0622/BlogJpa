060b2d45dfbe1ba3b8cdbdc0bd04db8e3ab1f7e9
dragon0622 <iamdragon.dev@gmail.com>
2024-03-17 23:39:22 +0900

    Feat: CommentRepository interface에 추상메소드 추가, repository annotation

--- src/main/java/com/estsoft/blogjpa/repository/BlogRepository.java
+++ src/main/java/com/estsoft/blogjpa/repository/BlogRepository.java
@@ -2,7 +2,9 @@ package com.estsoft.blogjpa.repository;
 
 import com.estsoft.blogjpa.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogRepository extends JpaRepository<Article, Long> {
     //상속을 받으면서 이 레파지토리를 사용할땓, JpaRepository에서 제공하는 여러 메소드 사용 가능
 }
--- src/main/java/com/estsoft/blogjpa/repository/CommentRepository.java
+++ src/main/java/com/estsoft/blogjpa/repository/CommentRepository.java
@@ -1,6 +1,12 @@
 package com.estsoft.blogjpa.repository;
 
+import com.estsoft.blogjpa.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CommentRepository extends JpaRepository {
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Comment findByArticleId(Long articleId, Long commentId);
 }
