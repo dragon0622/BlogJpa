e1317a73a8a4ca37549d8523bc867bfd2db64e97
dragon0622 <iamdragon.dev@gmail.com>
2024-03-19 14:35:08 +0900

    Fix: 오류해결 - sql 매개변수 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,6 +8,6 @@ INSERT INTO article (title, content, created_at, updated_at) VALUES ('블로그 
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('블로그 제목2', '블로그 내용2', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('블로그 제목3', '블로그 내용3', NOW(), NOW());
 
-INSERT INTO comment (article_id, body, created_at) VALUES (1, 'firstArticle_comment', NOW());
-INSERT INTO comment (article_id, body, created_at) VALUES (2, 'secondArticle_comment', NOW());
-INSERT INTO comment (article_id, body, created_at) VALUES (3, 'thirdArticle_comment', NOW());
+INSERT INTO comment (id, article_id, body, created_at) VALUES (1, 1, 'firstArticle_comment', NOW());
+INSERT INTO comment (id, article_id, body, created_at) VALUES (2, 2, 'secondArticle_comment', NOW());
+INSERT INTO comment (id, article_id, body, created_at) VALUES (3, 3, 'thirdArticle_comment', NOW());
