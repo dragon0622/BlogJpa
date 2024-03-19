59ff331d434f3511b7f921c398b87ae15b52db8b
dragon0622 <iamdragon.dev@gmail.com>
2024-03-19 15:36:14 +0900

    Feat: logout 구현

--- src/main/java/com/estsoft/blogjpa/controller/UserController.java
+++ src/main/java/com/estsoft/blogjpa/controller/UserController.java
@@ -2,7 +2,13 @@ package com.estsoft.blogjpa.controller;
 
 import com.estsoft.blogjpa.dto.AddUserRequest;
 import com.estsoft.blogjpa.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -17,5 +23,11 @@ public class UserController {
     public String signup(AddUserRequest request){         // json형태일때는 @RequsetBody 해줘야합니다.
         userService.save(request);
         return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest servletRequest, HttpServletResponse servletResponse){
+        new SecurityContextLogoutHandler().logout(servletRequest,servletResponse, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect/login";
     }
 }
