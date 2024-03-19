99c7a48884e1641c6d5926b2e86ed881c4c0f63a
dragon0622 <iamdragon.dev@gmail.com>
2024-03-19 09:57:56 +0900

    Feat: comment sql 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,7 @@
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('블로그 제목', '블로그 내용', NOW(), NOW()); --최초 생성시에도 생성시간, 수정시간을 수정할 수 있도록 sql 수정
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('블로그 제목2', '블로그 내용2', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('블로그 제목3', '블로그 내용3', NOW(), NOW());
+
+INSERT INTO comment (article_id, body, created_at) VALUES (1, 'firstArticle_comment', NOW());
+INSERT INTO comment (article_id, body, created_at) VALUES (2, 'secondArticle_comment', NOW());
+INSERT INTO comment (article_id, body, created_at) VALUES (3, 'thirdArticle_comment', NOW());
